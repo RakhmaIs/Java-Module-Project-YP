fb8d790016274898280e5681c59484fb7be73184
Iskhakov Shamil <forinfoiskhakov@gmail.com>
2024-02-28 17:11:29 +0300

    Technical task №4 - done

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Test{
+    public static void main(String[] args){
+
+
+        public static <T> List<T> doSmth(List<T> l) {
+            List<T> u = new ArrayList<>();
+
+        }
+    }
+}
